0d3b1659f9d3d390ea7071b4c3f6775a6b580a36
melhoridadesa <root@php501.4linux.com.br>
2016-05-19 22:21:40 -0300

    Migrates para criacao das tabelas: contato, conteudo, menu, idoso

--- database/migrations/2016_05_20_005744_contato.php
+++ database/migrations/2016_05_20_005744_contato.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Contato extends Migration
 {
     /**
      * Run the migrations.
@@ -12,13 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contato', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('tipo_acesso');
-            $table->rememberToken();
+            $table->string('email');
+            $table->string('nome');
+            $table->string('assunto');
+            $table->string('mensagem');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        //
     }
 }
