515036f468979ae84457d531b4408438384240e5
Ayrton <ayrton.figueira@gmail.com>
2016-05-19 22:27:02 -0300

    Ajustando Layout Admin

--- database/migrations/2016_05_20_002347_menu.php
+++ database/migrations/2016_05_20_002347_menu.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Celular extends Migration
+class Menu extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,10 @@ class Celular extends Migration
      */
     public function up()
     {
-        Schema::create('celular', function (Blueprint $table) {
+        Schema::create('menu', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('link');
             $table->string('nome');
-            $table->string('numero');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ class Celular extends Migration
      */
     public function down()
     {
-        Schema::drop('celular');
+        Schema::drop('menu');
     }
 }
