88c44fbe0b1cd964470b10c03874670df250f574
lukevictor <root@php501.4linux.com.br>
2016-05-24 22:12:24 -0300

    Menu do admin carregando apenas conteudo do admin

--- database/seeds/Menu.php
+++ database/seeds/Menu.php
@@ -14,18 +14,27 @@ class Menu extends Seeder
       DB::table('menu')->insert([
           'titulo' => 'Home',
           'link' => '/',
+          'tipo_acesso' => 'u',
       ]);
       DB::table('menu')->insert([
           'titulo' => 'Quem somos',
           'link' => '/quemsomos',
+          'tipo_acesso' => 'u',
       ]);
       DB::table('menu')->insert([
           'titulo' => 'Contato',
           'link' => '/contato',
+          'tipo_acesso' => 'u',
       ]);
       DB::table('menu')->insert([
           'titulo' => 'Login',
           'link' => '/login',
+          'tipo_acesso' => 'u',
+      ]);
+      DB::table('menu')->insert([
+          'titulo' => 'Cadastro',
+          'link' => '/cadastro',
+          'tipo_acesso' => 'a',
       ]);
     }
 }
